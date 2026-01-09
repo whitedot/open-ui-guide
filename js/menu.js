ff8b9478330c6faee2cbd57ec31cad6f15e58103
whitedot <whitedot@naver.com>
2026-01-09 15:07:53 +0900

    refactor: 메뉴에서 Form Group 항목 제거
    - Molecules 섹션에서 불필요한 Form Group 메뉴 항목 삭제

--- js/menu.js
+++ js/menu.js
@@ -28,7 +28,6 @@ const MENU_DATA = [
     {
         title: 'Molecules',
         items: [
-            { id: 'form-group', name: 'Form Group', href: 'molecules/form-group.html', icon: 'M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01' },
             { id: 'card', name: 'Card', href: 'molecules/card.html', icon: 'M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10' },
             { id: 'search-input', name: 'Search Input', href: 'molecules/search-input.html', icon: 'M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z' },
             { id: 'button-group', name: 'Button Group', href: 'molecules/button-group.html', icon: 'M4 6h16M4 10h16M4 14h16M4 18h16' },
